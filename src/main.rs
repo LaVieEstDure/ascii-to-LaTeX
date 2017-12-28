9b044ff2ba267851a223dcb262b6d051b3c2f539
LaVieEstDure <mishraraghav24@gmail.com>
2017-12-28 13:41:10 +1000

    Plane rides are a bore

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
+#![cfg_attr(feature="clippy", feature(plugin))]
+#![cfg_attr(feature="clippy", plugin(clippy))]
+
+
 mod token;
 mod lexer;
 mod tests;
 mod parser;
 
 
-
 fn main(){
-    let text = String::from("3.14+2");
+    let text = String::from("3.14 sin(3) +2");
     let x = lexer::Lexer::new(&text);
     let p = parser::parse(x.peekable());
     println!("{:?}", p);
--- src/parser.rs
+++ src/parser.rs
@@ -22,21 +22,40 @@ pub struct Parser<'a> {
 }
 
 pub fn get_priority(token: &Token) -> u32 {
-    match token {
-        &Token::Multiply | &Token::Divide => 3,
-        &Token::Plus | &Token::Minus => 2,
+    match *token {
+        Token::Multiply | Token::Divide => 3,
+        Token::Plus | Token::Minus => 2,
         _ => 1
+    }
+}
+
+pub fn is_operator(token: &Token) -> bool {
+    match *token {
+        Token::Plus
+        | Token::Minus
+        | Token::Multiply
+        | Token::Divide => true,
+        
+        _ => false
     }
 }
 
 pub fn parse<I> (mut eq: Peekable<I>) -> Expression 
     where I: Iterator<Item=Token> {
-    let first = match eq.next() {
-        Some(Token::Number(n)) => Expression::Number(n),
-        _ => Expression::Number(String::from(""))
-    };
-    match eq.next() {
-        Some(Token::Plus) => Expression::BinaryExpr(Box::new(first), Operation::Plus, Box::new(Expression::Number("2".to_string()))),
-        _ => Expression::Number("1".to_string())
+    let mut first: Vec<Token> = vec!(eq.next().unwrap());
+
+    while !is_operator((eq.peek().unwrap())){
+        first.push(eq.next().unwrap())
+    }
+
+    println!("{:?}", first);
+    Expression::Number("0".to_string())
+}
+
+pub fn parse_prefix_expr<I>(mut eq: Peekable<I>) -> Expression
+    where I: Iterator<Item=Token> {
+    match eq.next().unwrap() {
+        Token::Number(c) => Expression::Number(c),
+        _ => panic!("Error")
     }
 }
